238726db51d5095a6bc6e33bc829f51868d76cff
sharma021 <63513761+sharma021@users.noreply.github.com>
2020-07-23 22:57:43 +0530

    Create post.js

---
+++ post.js
@@ -0,0 +1,17 @@
+'use strict';
+const http = require('http');
+const PORT = 8080;
+const server = http.createServer((request, response) => {
+ let buffer = '';
+ request.on('data', chunk => {
+ buffer += chunk;
+ });
+ request.on('end', () => {
+ const responseString = `Received string ${buffer}`;
+ console.log(`Responding with: ${responseString}`);
+ response.writeHead(200, "Content-Type: text/plain");
+ response.end(responseString);
+ });
+}).listen(PORT, () => {
+ console.log(`Listening on ${PORT}`);
+});
