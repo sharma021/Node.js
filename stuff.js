c30c4925487ccd07e05692b31c9940b716b78e8f
sharma021 <63513761+sharma021@users.noreply.github.com>
2020-07-23 23:05:01 +0530

    Create stuff.js

---
+++ stuff.js
@@ -0,0 +1,12 @@
+var counter = function(arr){
+    return 'There are' + arr.length + 'elements in this array';
+};
+var adder=function(a,b){
+    return `the sum of the two numbers is ${a+b}`;
+};
+var pi=3.142;
+module.exports={
+    counter : counter,
+    adder : adder,
+    pi : pi
+}
